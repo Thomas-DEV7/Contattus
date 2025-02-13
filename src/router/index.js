703e907a01e7e5a68a388bead3299c66d51965e9
Thomas Bastos <thomas.felip16@gmail.com>
2025-02-12 22:28:01 -0300

    feat: add deps and routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,42 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Login from "@/views/Login.vue";
-import Home from "@/views/Home.vue";
+// router/index.js
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from '../views/Login.vue'
+import Home from '../views/Home.vue'
+import Profile from '../views/Profile.vue'
+import Users from '../views/Users.vue'
+import People from '../views/People.vue'
+import Contacts from '../views/Contacts.vue'
 
 const routes = [
-  { path: "/", component: Login },
-  { path: "/home", component: Home },
-];
+  { path: '/', redirect: '/login' },
+  { path: '/login', name: 'Login', component: Login, meta: { requiresGuest: true } },
+  { path: '/home', name: 'Home', component: Home, meta: { requiresAuth: true } },
+  { path: '/profile', name: 'Profile', component: Profile, meta: { requiresAuth: true } },
+  { path: '/users', name: 'Users', component: Users, meta: { requiresAuth: true, requiresAdmin: true } },
+  { path: '/people', name: 'People', component: People, meta: { requiresAuth: true } },
+  { path: '/contacts', name: 'Contacts', component: Contacts, meta: { requiresAuth: true } }
+  
+]
+
 
 const router = createRouter({
   history: createWebHistory(),
-  routes,
-});
+  routes
+})
 
-export default router;
+// Simple authentication guard based on localStorage (pode ser aprimorado utilizando o Pinia)
+router.beforeEach((to, from, next) => {
+  const user = JSON.parse(localStorage.getItem('user'))
+  
+  if (to.meta.requiresAuth && !user) {
+    next({ name: 'Login' })
+  } else if (to.meta.requiresGuest && user) {
+    next({ name: 'Home' })
+  } else if (to.meta.requiresAdmin && user && user.type !== 'Admin') {
+    next({ name: 'Home' })
+  } else {
+    next()
+  }
+})
+
+export default router
